f86caf56c1d84bd6a4df77464f25a1081c3f1011
Andres Felipe Pardo Mesa <a.pardo6162@gmail.com>
2018-05-11 19:24:57 -0500

    gitignore file added

---
+++ src/main/java/co/edu/escuelaing/arem/dianfacturaelectronica/security/authentication/authenticationValidate.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arem.dianfacturaelectronica.security.authentication;
+
+/**
+ *
+ * @author andres
+ */
+public class authenticationValidate {
+   
+    authenticationValidate(){
+        
+    }
+}
